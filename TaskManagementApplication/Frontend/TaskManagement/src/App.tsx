dab7a059f7a0119665ae1f10ce9bb81d960d541e
Shubham Agrawal <shubhamagrawal2904@gmail.com>
2025-11-22 22:42:40 +0530

    tailwind is install and setup for web app.

---
+++ TaskManagementApplication/Frontend/TaskManagement/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+    return (
+        <>
+            <h1 className="text-red-800 text-4xl font-bold">
+                Hello world
+            </h1>
+        </>
+    )
+}
+
+export default App
