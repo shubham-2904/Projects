9d941326151f8ed22b592eba77b3a7f6277809e8
Shubham Agrawal <shubhamagrawal2904@gmail.com>
2026-03-08 14:44:13 +0530

    Designed logger service for application

---
+++ Auction_Application/Backend/AuctionApp_Server/LoggerService/ILoggerManager.cs
@@ -0,0 +1,9 @@
+﻿namespace LoggerService;
+
+public interface ILoggerManager
+{
+    void LogInfo(string message);
+    void LogWarning(string message);
+    void LogError(string message);
+    void LogError(Exception ex, string message);
+}
---
+++ Auction_Application/Backend/AuctionApp_Server/LoggerService/LoggerManager.cs
@@ -0,0 +1,26 @@
+﻿using Serilog;
+
+namespace LoggerService;
+
+public class LoggerManager : ILoggerManager
+{
+    public void LogError(string message)
+    {
+        Log.Error(message);
+    }
+
+    public void LogError(Exception ex, string message)
+    {
+        Log.Error(ex, message);
+    }
+
+    public void LogInfo(string message)
+    {
+        Log.Information(message);
+    }
+
+    public void LogWarning(string message)
+    {
+        Log.Warning(message);
+    }
+}
